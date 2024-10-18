88c71daddbf3bf59ae55aa02d13372c47f97b0a8
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-10-18 11:03:56 -0300

    adicionando teclado

--- lib/pages/login_page_teacher.dart
+++ lib/pages/login_page_teacher.dart
@@ -21,150 +21,151 @@ class _LoginPageState extends State<LoginPage> {
       child: Scaffold(
         backgroundColor: Color.fromARGB(255, 17, 30, 58),
 
-        body: SizedBox(
-          width: double.infinity,
+        body:SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: MediaQuery.of(context).size.height,
+            ),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height:100 ,),
 
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height:100 ,),
+                  Container(
+                    // width: 150,
+                    width: 500,
+                    height: 250,
 
-              Container(
-                // width: 150,
-                width: 500,
-                height: 250,
+                    child: Row(
+                      children: [
+                        Expanded(child: Container(),),
+                        
+                        Expanded(
+                          flex: 10,
+                          child: Image.asset(
+                                  "assets/logo_png.png",
+                                ),
+                        ),
 
-                child: Row(
-                  children: [
-                    Expanded(child: Container(),),
+                        Expanded(child: Container(),),
+                      ],
+                    ),
+                  ),
+
+
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    // color: Color.fromARGB(255, 238, 112, 112),
+                    height: 50,
+                    width: double.infinity,
+                    alignment: Alignment.center,
+
+                    child:TextField(
+                      decoration: InputDecoration(
+                        contentPadding:EdgeInsets.only(top:0),
+                        hintText: 'Email..',
+                        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 255, 255, 255))),
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 238, 112, 112),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                        prefixIcon: Icon(
+                          Icons.person,
+                          size:38,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                      ),
+                    )
+                  ),
+
+                  SizedBox(height:10 ,),
+
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    // color: Color.fromARGB(255, 238, 112, 112),
+                    height: 50,
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    // child:Text("Informe a senha", style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w500)),),
+                    child:TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Password..',
+                        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 255, 255, 255))),
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 238, 112, 112),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                        suffixIcon: Icon(
+                          Icons.visibility,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                      ),
+                    )
                     
-                    Expanded(
-                      flex: 10,
-                      child: Image.asset(
-                              "assets/logo_png.png",
-                            ),
-                    ),
+                   
+                  ),
 
-                    Expanded(child: Container(),),
-                  ],
-                ),
-              ),
+                  SizedBox(height:30 ,),
 
-
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 30),
-                // color: Color.fromARGB(255, 238, 112, 112),
-                height: 50,
-                width: double.infinity,
-                alignment: Alignment.center,
-
-                child:TextField(
-                  decoration: InputDecoration(
-                    contentPadding:EdgeInsets.only(top:0),
-                    hintText: 'Email..',
-                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 255, 255, 255))),
-                    filled: true,
-                    fillColor: Color.fromARGB(255, 238, 112, 112),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(20)
-                    ),
-                    prefixIcon: Icon(
-                      Icons.person,
-                      size:38,
-                      color: Color.fromARGB(255, 255, 255, 255),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    // color: Colors.green,
+                    // height: 40,
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    
+                    child: SizedBox(
+                      width: double.infinity,
+                      child:TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))),
+                          backgroundColor: MaterialStateProperty.all(Colors.green),
+                        ),
+                        child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700),)
+                      ),
                     ),
                   ),
-                )
-              ),
 
-              SizedBox(height:10 ,),
+                  Expanded(child:Container() ,),
 
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 30),
-                // color: Color.fromARGB(255, 238, 112, 112),
-                height: 50,
-                width: double.infinity,
-                alignment: Alignment.center,
-                // child:Text("Informe a senha", style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w500)),),
-                child:TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Password..',
-                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 255, 255, 255))),
-                    filled: true,
-                    fillColor: Color.fromARGB(255, 238, 112, 112),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(20)
-                    ),
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                    ),
-                    suffixIcon: Icon(
-                      Icons.visibility,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                    ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    color: Color.fromARGB(255, 17, 30, 58),
+                    height: 40,
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    
+                    child: Text("Forget Password",  style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w200, decoration: TextDecoration.underline)),
                   ),
-                )
-                
-               
-              ),
 
-              SizedBox(height:30 ,),
+                  SizedBox(height:5 ,),
 
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 30),
-                // color: Colors.green,
-                // height: 40,
-                width: double.infinity,
-                alignment: Alignment.center,
-                
-                child: SizedBox(
-                  width: double.infinity,
-                  child:TextButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))),
-                      backgroundColor: MaterialStateProperty.all(Colors.green),
-                    ),
-                    child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700),)
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    color: Color.fromARGB(255, 17, 30, 58),
+                    height: 40,
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    
+                    child: Text("Get Started",  style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.w900)),
                   ),
-                ),
+
+                  SizedBox(height:60 ,),
+
+                ],
               ),
-
-              Expanded(child:Container() ,),
-
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 30),
-                color: Color.fromARGB(255, 17, 30, 58),
-                height: 40,
-                width: double.infinity,
-                alignment: Alignment.center,
-                
-                child: Text("Forget Password",  style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w200, decoration: TextDecoration.underline)),
-              ),
-
-              SizedBox(height:5 ,),
-
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 30),
-                color: Color.fromARGB(255, 17, 30, 58),
-                height: 40,
-                width: double.infinity,
-                alignment: Alignment.center,
-                
-                child: Text("Get Started",  style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.w900)),
-              ),
-
-              SizedBox(height:60 ,),
-
-            ],
           ),
+        ),
         )
-
-
-      )
     );
   }
 }
