ad9ae145affbc89b12a0fcf78f27959068009d89
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-10-22 12:41:06 -0300

    componente card

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -6,21 +8,69 @@ class CardPage extends StatefulWidget {
   const CardPage({Key? key}) : super(key: key);
 
   @override
-  State<CardPage> createState() => _Pagina1PageState();
+  State<CardPage> createState() => _CardPageState();
 }
 
-class _Pagina1PageState extends State<CardPage> {
+class _CardPageState extends State<CardPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor:Colors.green,
-
-      body:Center(
-        child: Container(
-
-          child:Text("Teste"),  
-        ),
-      )
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Card(
+            elevation: 8,
+            shadowColor: Colors.grey,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Image.network(
+                        height:20,
+                        "https://hermes.digitalinnovation.one/assets/diome/logo.png"
+                      ),
+                      Text(
+                        'Meu card',
+                        style: TextStyle(
+                          fontSize: 20, 
+                          fontWeight: FontWeight.w700
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Text(
+                    "O Gerador de Lero-lero para TI e informática foi baseado no Fabuloso Gerador de Lero-lero v2.0. Ele é capaz de gerar qualquer quantidade de texto vazio e prolixo, ideal para engrossar uma tese de mestrado, impressionar seu chefe ou preparar discursos capazes de curar a insônia da platéia. Basta informar um título pomposo qualquer (nos moldes do que está sugerido aí embaixo) e a quantidade de frases desejada. Voilá! Em dois nano-segundos você terá um texto - ou mesmo um livro inteiro - pronto para impressão. Ou, se preferir, faça copy/paste para um editor de texto para formatá-lo mais sofisticadamente. Lembre-se: aparência é tudo, conteúdo é nada.",
+                    style: TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.normal
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    // alignment:  Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {}, 
+                      child: Text(
+                        "Ler mais",
+                        style: TextStyle(
+                          fontSize: 16,
+                          decoration: TextDecoration.underline
+                        ),
+                      )
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+      
     );
   }
 }
