5feae759b6bb382d56b5786887bcde2544b420ed
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-11-14 12:41:37 -0300

    custom dio

--- lib/repositories/comments/impl/comments_dio_repository.dart
+++ lib/repositories/comments/impl/comments_dio_repository.dart
@@ -4,13 +4,14 @@ import 'package:dio/dio.dart';
 import 'package:trilhaap/model/comment_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:trilhaap/repositories/comments/comments_repository.dart';
+import 'package:trilhaap/repositories/jsonplaceholder_custom_dio.dart';
 
 
 class CommentsDioRepository implements CommentsRepository {
   @override
   Future<List<CommentModel>> retornaComentarios(int postId) async {
-    var dio = Dio();
-    var response = await dio.get("https://jsonplaceholder.typicode.com/posts/$postId/comments");
+    var jsonPlaceHolderCustomDio = JsonPlaceHolderCustomDio();
+    var response = await jsonPlaceHolderCustomDio.dio.get("posts/$postId/comments");
 
     if(response.statusCode == 200) {
       return (response.data as List).map((e) => CommentModel.fromJson(e)).toList();
